bed8127c9a193b2f63c702cc574f20cbc9ce1710
Damian Pukszta <damian@192.168.1.2>
2024-10-13 20:12:57 +0200

    Login method created

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,12 @@
 const user = require("../db/models/user");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+}
 
 const signup = async (req, res, next) => {
     const body = req.body;
@@ -19,6 +27,14 @@ const signup = async (req, res, next) => {
         birthDate: body.birthDate
     });
 
+    const result = newUser.toJSON();
+
+    delete result.password;
+
+    result.token = generateToken({
+        id: result.id,
+    })
+
     if(!newUser) {
         return res.status(400).json({
             status: 'fail',
@@ -28,8 +44,36 @@ const signup = async (req, res, next) => {
 
     res.status(201).json({
         status: 'success',
-        data: newUser
+        data: result
     });
 }
 
-module.exports = {signup}
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+       return res.status(400).json({
+            status: 'fail',
+            message: 'Please provide email and password'
+        });
+    }
+
+    const result = await user.findOne({where: {email}});
+    if(!result || !(await bcrypt.compare(password, result.password))) {
+        return res.status(401).json({
+            status: 'fail',
+            message: 'Incorrect email or password'
+        });
+    }
+
+    const token = generateToken({
+        id: result.id,
+    });
+
+    return res.json({
+        status: 'success',
+        token
+    })
+}
+
+module.exports = { signup, login }
